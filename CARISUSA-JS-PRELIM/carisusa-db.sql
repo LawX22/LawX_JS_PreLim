a9a3034e89f283b0ac658a7f2f1f51d52c14cf07
Lawrenz Xavier Carisusa <129159516+LawX22@users.noreply.github.com>
2024-03-27 00:43:33 +0800

    Add files via upload

--- CARISUSA-JS-PRELIM/carisusa-db.sql
+++ CARISUSA-JS-PRELIM/carisusa-db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2024 at 03:20 AM
+-- Generation Time: Mar 26, 2024 at 05:41 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `gullemdb`
+-- Database: `carisusa-db`
 --
 
 -- --------------------------------------------------------
@@ -37,13 +37,10 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`category_id`, `category_name`) VALUES
-(1, 'MC LAREN'),
-(2, 'LAMBORGHINI'),
-(3, 'FERRARI'),
-(4, 'KOENIGSEGG'),
-(5, 'MONEY'),
-(6, 'FORD'),
-(7, 'RUSI');
+(11, 'ASUS'),
+(12, 'ACER '),
+(13, 'MSI'),
+(14, 'RAZER BLADE');
 
 -- --------------------------------------------------------
 
@@ -55,7 +52,7 @@ CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
   `product_image` text NOT NULL,
   `product_name` varchar(255) NOT NULL,
-  `product_category` int(11) NOT NULL,
+  `product_category` varchar(255) NOT NULL,
   `product_quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -64,8 +61,13 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `product_image`, `product_name`, `product_category`, `product_quantity`) VALUES
-(7, 'uploads/McLaren 720S.png', 'MC LAREN', 1, 1),
-(8, 'uploads/inventory.png', 'lambo', 2, 2);
+(16, 'uploads/6602e600dd00d_f5c6343e2feecc67.jpg', 'ACER NITRO 7', 'ACER ', 3),
+(17, 'uploads/MSI THIN.jpg', 'MSI THIN GF63', 'MSI', 1),
+(18, 'uploads/6602e64c51e3a_5f8d96f1f67e8d3e.jpg', 'ACER PREDATOR', 'ACER ', 10),
+(19, 'uploads/6602e66f9eee8_4bdc6315ccf57252.jpg', 'RAZOR BLADE 15', 'RAZER BLADE', 1),
+(20, 'uploads/6602e6bb6cc6c_12b1ead432b84a07.jpg', 'ASUS ROG  ZEPHYRUS', 'ASUS', 1),
+(21, 'uploads/msi.jpg', 'MSI GL63', 'MSI', 5),
+(23, 'uploads/6602f42527dd7_3a0d353ef40efd3d.jpg', 'eafsef', 'MSI', 1);
 
 --
 -- Indexes for dumped tables
@@ -81,8 +83,7 @@ ALTER TABLE `category`
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`),
-  ADD KEY `product_category` (`product_category`);
+  ADD PRIMARY KEY (`product_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -92,23 +93,13 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `product`
---
-ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`product_category`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
